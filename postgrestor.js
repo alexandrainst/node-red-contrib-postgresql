8f29e7648a2ff03ed4b25ce20b0ef08e5fe35c1c
Yeray Medina Lopez <yeray.medina@extern.isban.com>
2018-03-09 14:25:50 +0100

    Little refactor to allow use async/await instead of co dependency

--- postgrestor.js
+++ postgrestor.js
@@ -1,48 +1,41 @@
 module.exports = function(RED) {
   'use strict';
   const mustache = require('mustache');
-  const PgPool = require('pg').Pool;
-  const co = require('co');
-
-  let pgPool = null;
+  const Pool = require('pg').Pool;
 
   function PostgresDBNode(n) {
-    let poolInstance = null;
-    RED.nodes.createNode(this, n);
-    const _getField = (kind, key) => {
-      let value = false;
-      const node = this;
+    const node = this;
+    const _getField = (kind, value) => {
       switch (kind) {
         case 'flow': {
-          value = node.context().flow.get(key);
+          return node.context().flow.get(value);
           break;
         }
         case 'global': {
-          value = node.context().global.get(key);
+          return node.context().global.get(value);
           break;
         }
         case 'num': {
-          value = parseInt(key);
+          return parseInt(value);
           break;
         }
         case 'bool': {
-          value = JSON.parse(key);
+          return JSON.parse(value);
           break;
         }
         default: {
-          value = key;
+          return value;
           break;
         }
       }
-      return value;
     };
-    const node = this;
+    RED.nodes.createNode(node, n);
     node.name = n.name;
     node.host = _getField(n.hostFieldType, n.host);
     node.hostFieldType = n.hostFieldType;
     node.port = _getField(n.portFieldType, n.port);
     node.portFieldType = n.portFieldType;
-    node.database = _getField(n.databaseFieldType, n.database);
+    node.database = _getField( n.databaseFieldType, n.database);
     node.databaseFieldType = n.databaseFieldType;
     node.ssl = _getField(n.sslFieldType, n.ssl);
     node.sslFieldType = n.sslFieldType;
@@ -50,35 +43,15 @@ module.exports = function(RED) {
     node.maxFieldType = n.maxFieldType;
     node.min = _getField(n.minFieldType, n.min);
     node.minFieldType = n.minFieldType;
-    node.idle = _getField(n.idleFieldType, n.idle);
+    node.idle = _getField( n.idleFieldType, n.idle);
     node.idleFieldType = n.idleFieldType;
+    // Check credentials
     if (node.credentials) {
       node.user = _getField(n.userFieldType, node.credentials.user);
       node.userFieldType = n.userFieldType;
       node.password = _getField(n.passwordFieldType, node.credentials.password);
       node.passwordFieldType = n.passwordFieldType;
     }
-    console.log(node);
-    class Pool extends PgPool {
-      constructor() {
-        if (!poolInstance) {
-          super({
-            user: node.user,
-            password: node.password,
-            host: node.host,
-            port: node.port,
-            database: node.database,
-            ssl: node.ssl,
-            max: node.max,
-            min: node.min,
-            idleTimeoutMillis: node.idle
-          });
-          poolInstance = this;
-        }
-        return poolInstance;
-      }
-    }
-    pgPool = new Pool();
   }
 
   RED.nodes.registerType('postgresDB', PostgresDBNode, {
@@ -90,38 +63,39 @@ module.exports = function(RED) {
 
   function PostgrestorNode(config) {
     const node = this;
-
     RED.nodes.createNode(node, config);
     node.topic = config.topic;
     node.config = RED.nodes.getNode(config.postgresDB);
-
-    node.on('input', function(msg) {
-      const template = {
-        msg: msg
-      };
-      co(
-        function* () {
-          let client = yield pgPool
-            .connect()
-            .catch((error) => node.error(error));
-          try {
-            msg.payload = yield client.query(
-              mustache.render(config.query, template)
-            );
-          } catch (error) {
-            node.error(error);
-            msg.err = error;
-          } finally {
-            node.send(msg);
-            client.release();
-          }
+    node.on('input', (msg) => {
+      const template = {msg};
+      let client = false;
+      (async () => {
+        try {
+          const query = mustache.render(config.query, template);
+          const pool = new Pool({
+            user: node.config.user,
+            password: node.config.password,
+            host: node.config.host,
+            port: node.config.port,
+            database: node.config.database,
+            ssl: node.config.ssl,
+            max: node.config.max,
+            min: node.config.min,
+            idleTimeoutMillis: node.config.idle
+          });
+          client = await pool.connect();
+          msg.payload = await client.query(query);
+        } catch (error) {
+          node.error(error);
+          msg.err = error;
+        } finally {
+          node.send(msg);
+          client && client.release();
         }
-      );
+      })();
     });
-
-    node.on('close', function() {
-      node.status({});
-    });
+    node.on('close', () => node.status({}));
   }
+
   RED.nodes.registerType('postgrestor', PostgrestorNode);
 };
