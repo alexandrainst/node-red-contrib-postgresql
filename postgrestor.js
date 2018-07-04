f458f023dcf1cd0bdc58087caed1d22b53750512
Yeray Medina López <ymedlop@gmail.com>
2018-07-04 13:07:02 +0200

    Update postgrestor.js

--- postgrestor.js
+++ postgrestor.js
@@ -1,70 +1,91 @@
 module.exports = function(RED) {
-  'use strict';
-  const mustache = require('mustache');
-  const Pool = require('pg').Pool;
+  "use strict";
+  const mustache = require("mustache");
+  const Pool = require("pg").Pool;
+
+  function getField(node, kind, value) {
+    switch (kind) {
+      case "flow": {
+        return node.context().flow.get(value);
+      }
+      case "global": {
+        return node.context().global.get(value);
+      }
+      case "num": {
+        return parseInt(value);
+      }
+      case "bool": {
+        return JSON.parse(value);
+      }
+      default: {
+        return value;
+      }
+    }
+  }
 
   function PostgresDBNode(n) {
     const node = this;
-    const _getField = (kind, value) => {
-      switch (kind) {
-        case 'flow': {
-          return node.context().flow.get(value);
-        }
-        case 'global': {
-          return node.context().global.get(value);
-        }
-        case 'num': {
-          return parseInt(value);
-        }
-        case 'bool': {
-          return JSON.parse(value);
-        }
-        default: {
-          return value;
-        }
-      }
-    };
     RED.nodes.createNode(node, n);
     node.name = n.name;
-    node.host = _getField(n.hostFieldType, n.host);
+    node.host = n.host;
     node.hostFieldType = n.hostFieldType;
-    node.port = _getField(n.portFieldType, n.port);
+    node.port = n.port;
     node.portFieldType = n.portFieldType;
-    node.database = _getField( n.databaseFieldType, n.database);
+    node.database = n.database;
     node.databaseFieldType = n.databaseFieldType;
-    node.ssl = _getField(n.sslFieldType, n.ssl);
+    node.ssl = n.ssl;
     node.sslFieldType = n.sslFieldType;
-    node.max = _getField(n.maxFieldType, n.max);
+    node.max = n.max;
     node.maxFieldType = n.maxFieldType;
-    node.min = _getField(n.minFieldType, n.min);
+    node.min = n.min;
     node.minFieldType = n.minFieldType;
-    node.idle = _getField( n.idleFieldType, n.idle);
+    node.idle = n.idle;
     node.idleFieldType = n.idleFieldType;
-    node.user = _getField(n.userFieldType, node.user);
+    node.user = n.user;
     node.userFieldType = n.userFieldType;
-    node.password = _getField(n.passwordFieldType, node.password);
+    node.password = n.password;
     node.passwordFieldType = n.passwordFieldType;
   }
 
-  RED.nodes.registerType('postgresDB', PostgresDBNode);
+  RED.nodes.registerType("postgresDB", PostgresDBNode);
 
   function PostgrestorNode(config) {
     const node = this;
     RED.nodes.createNode(node, config);
     node.topic = config.topic;
     node.config = RED.nodes.getNode(config.postgresDB);
-    node.on('input', (msg) => {
-      const query = mustache.render(config.query, {msg});
+    node.on("input", msg => {
+      const query = mustache.render(config.query, { msg });
+      const {
+        user,
+        password,
+        host,
+        port,
+        database,
+        ssl,
+        max,
+        min,
+        iddle,
+        userFieldType,
+        passwordFieldType,
+        hostFieldType,
+        portFieldType,
+        databaseFieldType,
+        sslFieldType,
+        maxFieldType,
+        minFieldType,
+        idleFieldType
+      } = node.config;
       const pool = new Pool({
-        user: node.config.user,
-        password: node.config.password,
-        host: node.config.host,
-        port: node.config.port,
-        database: node.config.database,
-        ssl: node.config.ssl,
-        max: node.config.max,
-        min: node.config.min,
-        idleTimeoutMillis: node.config.idle
+        user: getField(node, userFieldType, user),
+        password: getField(node, passwordFieldType, password),
+        host: getField(node, hostFieldType, host),
+        port: getField(node, portFieldType, port),
+        database: getField(node, databaseFieldType, database),
+        ssl: getField(node, sslFieldType, ssl),
+        max: getField(node, maxFieldType, max),
+        min: getField(node, minFieldType, min),
+        idleTimeoutMillis: getField(node, idleFieldType, iddle)
       });
       const asyncQuery = async () => {
         let client = false;
@@ -81,8 +102,8 @@ module.exports = function(RED) {
       };
       asyncQuery();
     });
-    node.on('close', () => node.status({}));
+    node.on("close", () => node.status({}));
   }
 
-  RED.nodes.registerType('postgrestor', PostgrestorNode);
+  RED.nodes.registerType("postgrestor", PostgrestorNode);
 };
