fe10179789dbb5463ec7ffe3dee162df3ac6479b
Alexandre Alapetite <alexandre@alapetite.fr>
2021-07-27 23:48:10 +0200

    Implement back-pressure

--- postgrestor.js
+++ postgrestor.js
@@ -1,5 +1,33 @@
+'use strict';
+
+/**
+ * Return an incoming node ID if the node has any input wired to it, false otherwise.
+ * If filter callback is not null, then this function filters incoming nodes.
+ * @param {Object} toNode
+ * @param {function} filter
+ * @return {(number|boolean)}
+ */
+function findInputNodeId(toNode, filter = null) {
+  if (toNode && toNode._flow && toNode._flow.global) {
+    const allNodes = toNode._flow.global.allNodes;
+    for (const fromNodeId of Object.keys(allNodes)) {
+      const fromNode = allNodes[fromNodeId];
+      if (fromNode.wires) {
+        for (const wireId of Object.keys(fromNode.wires)) {
+          const wire = fromNode.wires[wireId];
+          for (const toNodeId of wire) {
+            if (toNode.id === toNodeId && (!filter || filter(fromNode))) {
+              return fromNode.id;
+            }
+          }
+        }
+      }
+    }
+  }
+  return false;
+}
+
 module.exports = function (RED) {
-  'use strict';
   const mustache = require('mustache');
   const Cursor = require('pg-cursor');
   const Pool = require('pg').Pool;
@@ -71,46 +99,141 @@ module.exports = function (RED) {
     const node = this;
     RED.nodes.createNode(node, config);
     node.topic = config.topic;
+    node.query = config.query;
+    node.split = config.split;
+    node.rowsPerMsg = config.rowsPerMsg;
     node.config = RED.nodes.getNode(config.postgresDB);
-    node.on('input', (msg) => {
-      const query = mustache.render(config.query, { msg });
 
-      const asyncQuery = async () => {
-        let client = false;
+    // Declare the ability of this node to provide ticks upstream for back-pressure
+    node.tickProvider = true;
+    let tickUpstreamId;
+    let tickUpstreamNode;
+    let upstreamPartsId = '';
+
+    // Declare the ability of this node to consume ticks from downstream for back-pressure
+    node.tickConsumer = true;
+    let downstreamReady = true;
+
+    // For streaming from PostgreSQL
+    let cursor;
+    let getNextRows;
+
+    node.on('input', async (msg) => {
+      if (tickUpstreamId === undefined) {
+        tickUpstreamId = findInputNodeId(node, (n) => RED.nodes.getNode(n.id).tickConsumer);
+        tickUpstreamNode = tickUpstreamId ? RED.nodes.getNode(tickUpstreamId) : null;
+      }
+
+      if (msg.tick) {
+        downstreamReady = true;
+        if (getNextRows) {
+          getNextRows();
+        }
+      } else {
+        const query = mustache.render(node.query, { msg });
+
+        let client = null;
+
+        const handleDone = () => {
+          if (cursor) {
+            cursor.close();
+            cursor = null;
+          }
+          if (client) {
+            client.release(true);
+            client = null;
+          }
+          getNextRows = null;
+        };
+
+        const handleError = (err) => {
+          console.error(err);
+          const error = (err ? err.toString() : 'Unknown error!') + ' ' + query;
+          node.error(error);
+          handleDone();
+          msg.payload = error;
+          msg.parts = {
+            id: upstreamPartsId,
+            abort: true,
+          };
+          downstreamReady = false;
+          node.send(msg);
+        };
+
+        handleDone();
+        upstreamPartsId = (msg.parts && msg.parts.id) || '' + Math.random();
+        downstreamReady = true;
+
         try {
           client = await node.config.pgPool.connect();
 
-          if (config.split) {
-            const cursor = client.query(new Cursor(query, msg.params || []));
-            const getNextRows = (err, rows) => {
+          if (node.split) {
+            let partsIndex = 0;
+            cursor = client.query(new Cursor(query, msg.params || []));
+            const cursorcallback = (err, rows) => {
               if (err) {
-                throw err;
+                handleError(err);
               } else if (rows.length > 0) {
-                node.send(Object.assign(Object.assign({}, msg), {
-                  payload: (config.rowsPerMsg || 1) > 1 ? rows : rows[0],
+                downstreamReady = false;
+                node.send(Object.assign({}, msg, {
+                  payload: (node.rowsPerMsg || 1) > 1 ? rows : rows[0],
+                  parts: {
+                    id: upstreamPartsId,
+                    type: 'array',
+                    index: partsIndex,
+                  },
                 }));
-                cursor.read(config.rowsPerMsg || 1, getNextRows);
+                partsIndex++;
+                getNextRows();
+              } else {
+                // Complete
+                handleDone();
+
+                downstreamReady = false;
+                node.send(Object.assign({}, msg, {
+                  payload: [],
+                  parts: {
+                    id: upstreamPartsId,
+                    type: 'array',
+                    index: partsIndex,
+                    count: partsIndex + 1,
+                  },
+                  complete: true,
+                }));
+                if (tickUpstreamNode) {
+                  tickUpstreamNode.receive({ tick: true });
+                }
               }
             };
-            cursor.read(config.rowsPerMsg || 1, getNextRows);
+
+            getNextRows = () => {
+              if (downstreamReady) {
+                cursor.read(node.rowsPerMsg || 1, cursorcallback);
+              }
+            };
           } else {
-            msg.payload = await client.query(query, msg.params || []);
-            node.send(msg);
+            getNextRows = async () => {
+              try {
+                msg.payload = await client.query(query, msg.params || []);
+                handleDone();
+                downstreamReady = false;
+                node.send(msg);
+                if (tickUpstreamNode) {
+                  tickUpstreamNode.receive({ tick: true });
+                }
+              } catch (ex) {
+                handleError(ex);
+              }
+            };
           }
 
+          getNextRows();
         } catch (err) {
-          const error = err.toString();
-          node.error(error);
-          msg.payload = error;
-          node.send(msg);
-        } finally {
-          if (client) {
-            client.release();
-          }
+          handleError(err);
         }
-      };
-      asyncQuery();
+      }
     });
+
     node.on('close', () => node.status({}));
   }
 
