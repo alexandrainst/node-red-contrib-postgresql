fc4b284710ed06debec850ae6746f9a8d3758ed2
Tedde Lundgren <mail@tedeh.net>
2021-09-29 20:58:33 +0700

    Fixes to the configuration object (#6)
    * Fix env configuration type
    
    * Required fields in config are no longer required
    
    * Remove wonky changing of password attribute
    
    * Update postgresql.html
    
    Co-authored-by: Alexandre Alapetite <alexandre@alapetite.fr>

--- postgresql.js
+++ postgresql.js
@@ -42,6 +42,8 @@ module.exports = function (RED) {
 				return parseInt(value);
 			case 'bool':
 				return JSON.parse(value);
+			case 'env':
+				return process.env[value];
 			default:
 				return value;
 		}
